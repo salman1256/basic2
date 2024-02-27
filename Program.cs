a52ecaf18da187c391f542007ca4a662ed23a7fa
salman1256 <salman1256@yahoo.com>
2024-02-27 17:02:20 +0530

    FinalCommit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+for(int num1=1;num1<=10;num1++)
+{
+    Console.WriteLine(num1);
+    Thread.Sleep(1000);
+}
+Console.WriteLine("10 to 100");
+for (int num1 = 10; num1 <= 100; num1++)
+{
+    Console.WriteLine(num1);
+    Thread.Sleep(100);
+}
